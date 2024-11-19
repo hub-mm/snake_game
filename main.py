33883a289a0c21bf510588f92d06efa685af55db
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-11-19 22:24:20 +0000

    Add main file and basic setup

---
+++ main.py
@@ -0,0 +1,35 @@
+# ./main.py
+
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOUR = '#00FF00'
+FOOD_COLOUR = '#FF0000'
+BACKGROUND_COLOUR = '#000000'
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction():
+    pass
+
+def check_collision(new_direction):
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+
+window.mainloop()
